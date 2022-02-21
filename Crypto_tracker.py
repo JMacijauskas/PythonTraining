4480f4ce9c0c71f873c321cde38b514d9ac8dd97
Justas Macijauskas <justas.macijauskas@auriga.com>
2022-02-21 19:55:58 +0200

    Added command line support for commands -p, -c, -cr, -t, -s

--- Crypto_tracker.py
+++ Crypto_tracker.py
@@ -4,7 +4,8 @@ import requests
 import os
 from twilio.rest import Client
 from dotenv import load_dotenv
-import sys
+import argparse
+import logging
 
 
 load_dotenv()  # Load .env stored environment variables in root
@@ -22,6 +23,7 @@ higher limits
 background functionality
 server
 """
+# TODO: add error handling
 
 
 # Named tuple class for data of alarms
@@ -43,21 +45,67 @@ class Alarm(typing.NamedTuple):
         )
 
 
-class SysArgs:
-    def __init__(self, passed_args):
-        self.args = passed_args
-        self.args_dict = {
-            'period': '-p',
-            'crypto_list': '-c',
-            'currencies': '-cr',
-            'threshold': '-t'
+def parse_args() -> argparse.Namespace:
+    args_dict = {
+        'period': {
+            'name1': '-p',
+            'name2': '--period',
+            'help': 'Time how often requests will be issued, in seconds. (default: %(default)s)',
+            'default': 60,
+            'type': int
+        },
+        'crypto': {
+            'name1': '-c',
+            'name2': '--crypto',
+            'help': 'List of crypto currency short names, that will be tracked. (default: %(default)s)',
+            'default': ('BTC', 'ETH', 'XRP'),
+            'type': str,
+            'nargs': '*'
+        },
+        'currencies': {
+            'name1': '-cr',
+            'name2': '--currencies',
+            'help': 'List of currency short names, that will be used for evaluation. (default: %(default)s)',
+            'default': ('USD', 'EUR'),
+            'type': str,
+            'nargs': '*'
+        },
+        'threshold': {
+            'name1': '-t',
+            'name2': '--threshold',
+            'help': 'Percentage value of threshold for tracking signals. (default: %(default)s)',
+            'default': 10,
+            'type': float
+        },
+        'sms': {
+            'name1': '-s',
+            'name2': '--sms',
+            'help': 'Whether or not sms notification should be sent. (default: %(default)s)',
+            'default': 0,
+            'type': int
         }
-
-    def set_period(self):
-        for possible_arg in self.args:
-            for arg in self.args_dict:
-                if arg == possible_arg:
-                    pass
+    }
+    parser = argparse.ArgumentParser()
+    for arg in args_dict:
+        if 'nargs' in args_dict[arg].keys():
+            parser.add_argument(
+                args_dict[arg]['name1'],
+                args_dict[arg]['name2'],
+                help=args_dict[arg]['help'],
+                default=args_dict[arg]['default'],
+                type=args_dict[arg]['type'],
+                nargs=args_dict[arg]['nargs']
+            )
+        else:
+            parser.add_argument(
+                args_dict[arg]['name1'],
+                args_dict[arg]['name2'],
+                help=args_dict[arg]['help'],
+                default=args_dict[arg]['default'],
+                type=args_dict[arg]['type']
+            )
+    args = parser.parse_args()
+    return args
 
 
 class CryptoTracker:
@@ -65,23 +113,22 @@ class CryptoTracker:
     API to track crypto can be accessed:
     https://www.cryptocompare.com/cryptopian/api-keys
     """
-    def __init__(self):
+    def __init__(self, currency_iter: typing.Iterable[str], crypto_coins: typing.Iterable[str], limit: int):
         self.crypto_api_key = os.getenv('CRYPTO_API_KEY')
-        self.currencies = ('USD', 'EUR')
-        self.crypto_names = ('BTC', 'ETH', 'XRP')
+        self.currencies = currency_iter
+        self.crypto_names = crypto_coins
         self.crypt_endpoint = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms={}&tsyms={}&api_key={}'.format(
             ','.join(self.currencies),
             ','.join(self.crypto_names),
             self.crypto_api_key
         )
         self.init_data = None
-        # self.period = 60
-        self.threshold = 0.001
+        self.threshold = limit
 
     def get_crypto_data(self) -> dict:
         response = requests.get(self.crypt_endpoint)
         changed_resp = self.parse_values(response.json())
-        print(changed_resp)
+        # print(changed_resp)
         return changed_resp
 
     @staticmethod
@@ -116,38 +163,30 @@ class Sender:
     Twillio API used to send sms can be accessed:
     https://www.twilio.com/console/projects/summary
     """
-    def __init__(self):
+    def __init__(self, bool_send):
         self.account_sid_twill = os.getenv('TWILIO_ACCOUNT_SID')
         self.secret_key_twill = os.getenv('TWILIO_AUTH_TOKEN')
         self.num_twill = os.getenv('TWILIO_NUMBER')
         self.user_number = os.getenv('USER_NUMBER')
         self.message_lim = 1600
         self.template_sms = ''
+        self.sms_send = bool_send
 
-    def sending(self, data):
-        data_text = self.process_alarm_data(data)
-        quants = self.quantify(data_text)
+    def sending(self, data: list[Alarm]) -> None:
+        alarm_str_generator = (str(alarm) for alarm in data)  # generator
+        quants = self.quantify(alarm_str_generator)
         client = Client(self.account_sid_twill, self.secret_key_twill)
         for message in quants:
             print(message)
-            # message_sent = client.messages.create(
-            #     body=f'{message}',
-            #     from_=f'{self.num_twill}',
-            #     to=f'{self.user_number}'
-            # )
-            # print(message_sent.sid)
+            if self.sms_send:
+                message_sent = client.messages.create(
+                    body=f'{message}',
+                    from_=f'{self.num_twill}',
+                    to=f'{self.user_number}'
+                )
+                print(message_sent.sid)
 
-    @staticmethod
-    def process_alarm_data(alarms: list[Alarm]) -> list:
-        # takes list of data with alarm class for each coin:
-        # crypto name, currency, current value, initial value, change %
-        text_alarms = []
-        for alarm in alarms:
-            alarm_str = f'{alarm}'
-            text_alarms.append(alarm_str)
-            return text_alarms
-
-    def quantify(self, alarms: list):
+    def quantify(self, alarms: typing.Generator[str, None, None]) -> typing.Generator[str, None, None]:
         full_message = ''
         for alarm in alarms:
             if len(alarm) + len(full_message) < self.message_lim:
@@ -159,15 +198,15 @@ class Sender:
 
 
 if __name__ == '__main__':
-    period = 60
-    print(sys.argv)
+    arguments = parse_args()
+    period = arguments.period
 
-    new_track = CryptoTracker()
-    sendin = Sender()
+    new_track = CryptoTracker(arguments.currencies, arguments.crypto, arguments.threshold)
+    sendin = Sender(arguments.sms)
     while True:
         new_vals = new_track.get_crypto_data()
         alarms_in_list = new_track.check_for_alarms(new_vals)
-        if len(alarms_in_list) > 0:
+        if alarms_in_list:
             sendin.sending(alarms_in_list)
         print(f'wait {period}s')
         time.sleep(period)
