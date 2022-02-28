216776ceebf596a05241a829c695342eaf6528f9
Justas Macijauskas <justas.macijauskas@auriga.com>
2022-02-28 03:52:41 +0200

    Tried to play with Qt5, questions remain for now

--- Typing_racer/Typing_racer.py
+++ Typing_racer/Typing_racer.py
@@ -1,5 +1,7 @@
 import datetime
 import time
+import sys
+from PySide2.QtWidgets import QLineEdit, QPushButton, QApplication, QVBoxLayout, QDialog, QTextEdit
 
 """
 display set text
@@ -12,9 +14,36 @@ return results (time diff, word count)
 """
 
 
+class Form(QDialog):
+    def __init__(self, samp_text, parent=None):
+        super(Form, self).__init__(parent)
+        self.setWindowTitle("Typing racer")
+        # widgets
+        self.sample_text = QTextEdit(samp_text)
+        self.sample_text.setReadOnly(True)
+        self.edit = QLineEdit()
+        self.button_sub = QPushButton("Submit")
+        self.button_st = QPushButton("Start")
+        # Create layout and add widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.button_st)
+        layout.addWidget(self.sample_text)
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button_sub)
+
+        # Set dialog layout
+        self.setLayout(layout)
+        # Add button signal to greetings slot
+        self.wtf = self.button_sub.clicked.connect(self.submit)
+        print(self.wtf)
+
+    # Greets the user
+    def submit(self):
+        return 'ok'
+
+
 def counter(time_wait):
     for sec in range(time_wait + 1):
-        print(sec)
         time.sleep(1)
 
 
@@ -36,4 +65,12 @@ text_sample = ('There are only two ways to live your life. One is as though noth
                ' The other is as though everything is a miracle.')
 
 if __name__ == '__main__':
-    run(text_sample)
+    # run(text_sample)
+
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+    # Create and show the form
+    form = Form(text_sample)
+    form.show()
+    # Run the main Qt loop
+    sys.exit(app.exec_())
